1863623f0233b9e28ba79aa468a762bd4fe6b3b5
Dominique Charlebois <domo.charlebois@gmail.com>
2019-06-23 11:38:34 -0700

    Revert change to app.run()

--- app.py
+++ app.py
@@ -83,4 +83,9 @@ def search():
   return jsonify(tweets=tweets)
 
 if __name__ == '__main__':
-  app.run()
+  port = int(os.environ.get("PORT", 5000))
+
+  if port == 5000:
+    app.debug = True
+
+  app.run(host='0.0.0.0', port=port)
