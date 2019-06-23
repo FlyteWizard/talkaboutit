2faf1472b1e0447cd6ee716216be0502d1d04d94
Dominique Charlebois <domo.charlebois@gmail.com>
2019-06-23 11:33:48 -0700

    Change to app.run()

--- app.py
+++ app.py
@@ -83,9 +83,4 @@ def search():
   return jsonify(tweets=tweets)
 
 if __name__ == '__main__':
-  port = int(os.environ.get("PORT", 5000))
-
-  if port == 5000:
-    app.debug = True
-
-  app.run(host='0.0.0.0', port=port)
+  app.run()
